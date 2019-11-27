0c8ea185a4cf7ffb023c3891ac03a20720ab9f1f
ivelebedev <ivelebedev@gmail.com>
2019-11-27 15:10:48 +0300

    Updates

---
+++ precache-manifest.806893616d176d591f20632365103c09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b70c0b6bbd1468df24c123925d37a0",
+    "url": "/test_image_uploader/index.html"
+  },
+  {
+    "revision": "8889277664e899feb0c6",
+    "url": "/test_image_uploader/static/css/main.60487dd4.chunk.css"
+  },
+  {
+    "revision": "68c58c824e1aacb3b3cf",
+    "url": "/test_image_uploader/static/js/2.c475d84c.chunk.js"
+  },
+  {
+    "revision": "8889277664e899feb0c6",
+    "url": "/test_image_uploader/static/js/main.f2f77d76.chunk.js"
+  },
+  {
+    "revision": "265ec7b05c9e272ebad5",
+    "url": "/test_image_uploader/static/js/runtime-main.612b18be.js"
+  }
+]);
